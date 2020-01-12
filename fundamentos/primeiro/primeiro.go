0b1b6488ea935ee941a804c801da5f3d3bd9b026
Higor Diego <higordiegoti@gmail.com>
2020-01-11 22:59:30 -0300

    fundamentos :

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+// iniciar executavel
+package main
+
+/*
+	Os códigos são organizados em pacotes
+	e para usuá-los é necessários declara um ou vários imports
+*/
+import "fmt"
+
+func main() {
+	fmt.Println("Testando o print")
+	fmt.Println("Testando o print")
+}
+ 
