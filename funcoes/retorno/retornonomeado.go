f462f06a932517b0f85f324ab84097448865b3e9
Higor Diego <higordiegoti@gmail.com>
2020-01-16 20:18:23 -0300

    modificando retorno de funções

--- funcoes/retorno/retornonomeado.go
+++ funcoes/retorno/retornonomeado.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func trocar(p1, p2 int) (segundo int, primeiro int) {
+func trocar(p1, p2 int) (segundo, primeiro int) {
 	segundo = p2
 	primeiro = p1
 	return // retorno limpo.
