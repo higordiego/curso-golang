87681de565de46ffc905d157649189ed82ba3848
Higor Diego <higordiegoti@gmail.com>
2020-01-22 19:32:47 -0300

    interface

---
+++ tipos/composicao/composicao.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+}
+
+type bmw7 struct{}
+
+func (b bmw7) fazerBaliza() {
+	fmt.Println("baliza...")
+}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("Ligar Turbo")
+}
+
+func main() {
+	var b esportivoLuxuoso = bmw7{}
+	b.ligarTurbo()
+	b.fazerBaliza()
+}
