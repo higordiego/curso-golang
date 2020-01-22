5c9154ce65b89d97a7b9ae3c7d882d0a997a70e1
Higor Diego <higordiegoti@gmail.com>
2020-01-22 08:49:27 -0300

    interface

---
+++ tipos/interface2/interface2.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type esportiva interface {
+	ligarTurbo()
+}
+
+type ferrari struct {
+	modelo          string
+	turboLigado     bool
+	velocidadeAtual int
+}
+
+func (f *ferrari) ligarTurbo() {
+	f.turboLigado = true
+
+}
+
+func main() {
+	carro1 := ferrari{"F40", false, 0}
+	carro1.ligarTurbo()
+
+	var carro2 esportiva = &ferrari{"F40", false, 0}
+	carro2.ligarTurbo()
+
+	fmt.Println(carro1, carro2)
+}
