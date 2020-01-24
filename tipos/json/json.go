dcc87148271cc4c22526e0d5c8b344e39b409532
Higor Diego <higordiegoti@gmail.com>
2020-01-24 20:53:30 -0300

    identando json

--- tipos/json/json.go
+++ tipos/json/json.go
@@ -5,23 +5,24 @@ import (
 	"fmt"
 )
 
+
 type produto struct {
 	ID    int      `json:"id"`
-	Nome  string   `json:"nome"`
+	Nome string   `json:"nome"`
 	Preco float64  `json:"preco"`
-	Tag   []string `json:"tags"`
+	Tag  []string `json:"tags"`
 }
 
 func main() {
 	// struct para json
-
-	p1 := produto{1, "Notebook", 18999.90, []string{"Promocao", "Eltronico"}}
+p1 := produto{1, "Notebook", 18999.90, []string{"Promocao", "Eltronico"}}
 
 	p1Json, _ := json.Marshal(p1)
 
 	fmt.Println(string(p1Json))
 
 	var p2 produto
+
 
 	jsonString := `{"id": 2, "nome": "Caneta", "preco": 8.9, "tags": ["Papelaria", "Importando"]}`
 
